0afa6084c1ebc0a21127772732818203468fa78c
Andrey Lozovik <tuoboy@gmail.com>
2023-05-16 22:53:21 +0300

    Подключили react и добавили scss loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,14 +1,25 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-
+    // Если не используется typescript - нужен babel-loader
     const typescriptLoader = {
             test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/,
         }
+    const scssLoader = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        }
 
     return [
-        typescriptLoader,
+        typescriptLoader, scssLoader
     ]
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import './Counter.scss'
+
+export const Counter = () => {
+
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    return (
+       <div>
+           <button onClick={increment}>Tap</button>
+           <input value={count} disabled/>
+       </div>
+    );
+};
+
