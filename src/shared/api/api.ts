e9ffa35106133981eed6bc18b5d859adf7fe45f6
lo_and <tuoboy@gmail.com>
2023-11-04 16:39:17 +0300

    fix api

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -6,6 +6,7 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 export const $api = axios.create({
     baseURL: __API__,
     headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY), // была ошибка, добавил в null в Record
+        // @ts-ignore
+        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY),
     },
 });
