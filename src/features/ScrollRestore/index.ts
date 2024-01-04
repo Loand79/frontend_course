6fea5540e1c977c5a56733ec1759b87884019ac2
lo_and <tuoboy@gmail.com>
2024-01-04 15:28:35 +0300

    add Scroll state, scroll, restore

---
+++ src/features/ScrollRestore/index.ts
@@ -0,0 +1,5 @@
+export { getScrollPosition } from './model/selectors/getScrollPosiotion';
+
+export { ScrollRestoreSchema } from './model/types/ScrollRestoreSchema';
+
+export { scrollRestoreActions, scrollRestoreReducer } from './model/slices/scrollRestoreSlice';
