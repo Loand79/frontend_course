346c19eba13b3cba097b771dcb8ca1f4a7be475e
lo_and <tuoboy@gmail.com>
2024-01-29 01:05:19 +0300

    add admin page, forbidden page

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -2,3 +2,5 @@ export { userReducer, userActions } from './model/slice/userSlice';
 export { UserSchema, User } from './model/types/user';
 export { getUserAuthData } from './model/selectors/getUserAuthData';
 export { getUserInited } from './model/selectors/getUserInited/getUserInited';
+
+export { isUserAdmin, getUserRoles, isUserManager } from './model/selectors/roleSelectors';
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -6,9 +6,13 @@ import { ProfilePage } from 'pages/ProfilePage';
 import { ArticlesPage } from 'pages/ArticlesPage';
 import { ArticlesDetailsPage } from 'pages/ArticlesDetailsPage';
 import { ArticleEditPage } from 'pages/ArticleEditPage';
+import { UserRole } from 'entities/User/model/types/user';
+import { AdminPanelPage } from 'pages/AdminPanelPage';
+import ForbiddenPage from 'pages/ForbiddenPage/ui/ForbiddenPage';
 
 export type AppRouteProps = RouteProps & {
     authOnly?: boolean;
+    role?: UserRole[];
 }
 
 export enum AppRoutes {
@@ -20,6 +24,8 @@ export enum AppRoutes {
     ARTICLE_NEW = 'article_new',
     ARTICLE_EDIT = 'article_edit',
     NOT_FOUND = 'not_found',
+    ADMIN_PANEL = 'admin_panel',
+    FORBIDDEN = 'forbidden',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
@@ -30,6 +36,8 @@ export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.ARTICLE_DETAILS]: '/articles/',
     [AppRoutes.ARTICLE_EDIT]: '/articles/:id/edit',
     [AppRoutes.ARTICLE_NEW]: '/articles/new',
+    [AppRoutes.ADMIN_PANEL]: '/admin',
+    [AppRoutes.FORBIDDEN]: '/forbidden',
     [AppRoutes.NOT_FOUND]: '*',
 };
 
@@ -66,6 +74,17 @@ export const routeConfig: Record<AppRoutes, AppRouteProps> = {
         path: `${RoutePath.article_new}`,
         element: <ArticleEditPage />,
         authOnly: true,
+    },
+    [AppRoutes.ADMIN_PANEL]: {
+        path: `${RoutePath.admin_panel}`,
+        element: <AdminPanelPage />,
+        authOnly: true,
+        role: ['ADMIN', 'MANAGER'],
+
+    },
+    [AppRoutes.FORBIDDEN]: {
+        path: `${RoutePath.forbidden}`,
+        element: <ForbiddenPage />,
     },
     [AppRoutes.NOT_FOUND]: {
         path: RoutePath.not_found,
